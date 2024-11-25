dc52292d9b9eca6d11d49037f6a4912a8f3dc52e
ja-cooke <89166278+ja-cooke@users.noreply.github.com>
2024-11-25 13:12:02 +0000

    Project as submitted for coursework.

---
+++ PhysiBoard/GameViewController.swift
@@ -0,0 +1,51 @@
+//
+//  GameViewController.swift
+//  PhysiBoard
+//
+//  Single view app rendering a single SpriteKit scene
+//  Designed to run in landscape on the iPad (9th Generation)
+//  Written in Xcode 13.4.1 for iPad OS 15.7
+//
+//  Created by Jonathan Cooke on 17/01/2023.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "SynthLayoutScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
+
